ab21149be71edd42c2896634e0f7fcd2ce7c9bee
zheka <zheka@HOME-PC>
2024-05-21 16:48:35 +0300

    Classwork 2024 05 18 DONE

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
+#include <stack>
 using namespace std;
+using std::cout;
+//#define DINAMIC_MEMORY_1
+#define DINAMIC_MEMORY_2
+
 #define tab "\t"
+
 void FillRand(int arr[], const int n);
+void FillRand(int** arr, const int rows,const int cols);
+
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* push_back(int arr[], int& n, const int value);
 int* push_front(int arr[], int& n, const int value);
@@ -16,6 +25,7 @@ int* erase(int arr[], int& n, const int index);
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef DINAMIC_MEMORY_1
 	int n = 5;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n];
@@ -25,17 +35,17 @@ void main()
 
 	int value;
 	cout << "Введите добавляемое значение: "; cin >> value;
-	arr=push_back(arr, n, value);
+	arr = push_back(arr, n, value);
 	Print(arr, n);
-	
+
 	cout << "Введите добавляемое значение: "; cin >> value;
 	arr = push_front(arr, n, value);
 	Print(arr, n);
-	
+
 	arr = pop_back(arr, n);
 	Print(arr, n);
 
-	arr=pop_front(arr, n);
+	arr = pop_front(arr, n);
 	Print(arr, n);
 	int index;
 	cout << "Введите добавляемое значение: "; cin >> value;
@@ -44,13 +54,35 @@ void main()
 	Print(arr, n);
 
 	cout << "Введите индекс удаляемого числа: "; cin >> index;
-	arr=erase(arr, n,--index);
+	arr = erase(arr, n, --index);
 	Print(arr, n);
 
 
 
 
 	delete[] arr;
+#endif // DINAMIC_MEMORY_1
+
+	int rows = 3;
+	int cols = 4;
+	//////////////////////////////////////////////////////////////////////////
+   /////          Обьявление двумерного динамического массива           /////
+  //////////////////////////////////////////////////////////////////////////
+
+     //1) Создаём массив указателей:
+	int** arr = new int* [rows];
+
+	//2) Выделяем память под строки:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+	//////////////////////////////////////////////////////////////////////////
+   /////     Обращение к элементам двумерного динамического массива     /////
+  //////////////////////////////////////////////////////////////////////////
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	
 }
 void FillRand(int arr[], const int n)
 {
@@ -59,6 +91,18 @@ void FillRand(int arr[], const int n)
 		*(arr + i) = rand() % 100;
 	}
 }
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for(int i = 0; i < rows; i++)
+	{
+		for(int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+		cout << endl;
+	}
+}
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -66,6 +110,17 @@ void Print(int arr[], const int n)
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
 }
 int* push_back(int arr[],int& n,const int value)
 {
